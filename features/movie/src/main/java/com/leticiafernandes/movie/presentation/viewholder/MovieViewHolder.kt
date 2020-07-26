6a2c0772cffb49852c89fac41cb1f528a06ca880
Leticia Fernandes <leticiamara.fernandes@gmail.com>
2020-07-26 14:32:20 -0300

    Added coil lib to load movie post and show movie release date

--- features/movie/src/main/java/com/leticiafernandes/movie/presentation/viewholder/MovieViewHolder.kt
+++ features/movie/src/main/java/com/leticiafernandes/movie/presentation/viewholder/MovieViewHolder.kt
@@ -2,6 +2,10 @@ package com.leticiafernandes.movie.presentation.viewholder
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
+import com.leticiafernandes.movie.BuildConfig.MOVIE_IMAGE_BASE_URL
+import com.leticiafernandes.movie.R
+import com.leticiafernandes.movie.extensions.formatToReleaseDate
 import com.leticiafernandes.movie.presentation.model.MovieItem
 import kotlinx.android.synthetic.main.item_movie.view.*
 
@@ -10,12 +14,14 @@ class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bindMovie(movie: MovieItem, funFavouriteClickListener: (MovieItem) -> Unit,
                   funItemClickListener: (MovieItem) -> Unit) {
         itemView.textMovieTitle.text = movie.title
-        //TODO itemView.textReleaseDate.text = movie.releaseDate.formatToReleaseDate()
+        itemView.textReleaseDate.text = movie.releaseDate.formatToReleaseDate()
         itemView.textGenre.text = movie.genres.toString()
         itemView.textVoteAverage.text = movie.voteAverage.toString()
         itemView.fabFavourite.setOnClickListener { funFavouriteClickListener(movie) }
         itemView.setOnClickListener { funItemClickListener(movie) }
-        // TODO loadImage(itemView.context, movie.posterPath, itemView.imagePoster!!)
+        itemView.imagePoster.load(MOVIE_IMAGE_BASE_URL + movie.posterPath) {
+            placeholder(R.drawable.ic_popcorn)
+        }
         if (movie.favourite) itemView.fabFavourite.visibility = View.GONE
     }
 }
