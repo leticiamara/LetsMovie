67a006a11b6c6148dbe988a2a408355e461838c2
Leticia Fernandes <leticiamara.fernandes@gmail.com>
2020-09-06 19:10:11 -0300

    Show movie genres on movie list screen

--- features/movie/src/main/java/com/leticiafernandes/movie/domain/mapper/MovieMapper.kt
+++ features/movie/src/main/java/com/leticiafernandes/movie/domain/mapper/MovieMapper.kt
@@ -1,21 +1,30 @@
 package com.leticiafernandes.movie.domain.mapper
 
+import com.leticiafernandes.movie.domain.model.Genre
 import com.leticiafernandes.movie.domain.model.Movie
 import com.leticiafernandes.movie.domain.model.MovieResult
 import com.leticiafernandes.movie.presentation.model.MovieItem
 import com.leticiafernandes.movie.presentation.model.MovieResultItem
 
-fun MovieResult.mapToMovieResultItem(): MovieResultItem {
+fun MovieResult.mapToMovieResultItem(moviesGenres: List<Genre>): MovieResultItem {
     this.apply {
-        return MovieResultItem(page, totalResults, totalPages, mapToMovieItemList(results))
+        return MovieResultItem(
+                page,
+                totalResults,
+                totalPages,
+                mapToMovieItemList(results, moviesGenres)
+        )
     }
 }
 
-private fun mapToMovieItemList(movieList: List<Movie>): List<MovieItem> {
-    return movieList.map { mapToMovieItem(it) }
+private fun mapToMovieItemList(movieList: List<Movie>, moviesGenres: List<Genre>): List<MovieItem> {
+    return movieList.map {
+        val movieGenreList = moviesGenres.filter { genre -> genre.id in it.genreIds }
+        mapToMovieItem(it, movieGenreList)
+    }
 }
 
-private fun mapToMovieItem(movie: Movie): MovieItem {
+private fun mapToMovieItem(movie: Movie, movieGenreList: List<Genre>): MovieItem {
     movie.apply {
         return MovieItem(
                 id,
@@ -32,7 +41,7 @@ private fun mapToMovieItem(movie: Movie): MovieItem {
                 adult,
                 overview,
                 releaseDate,
-                listOf() //TODO Get gender list
+                movieGenreList.map { it.mapToGenreItem() }
         )
     }
 }
